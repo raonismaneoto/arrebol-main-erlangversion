ed885c4996decf7d440059c15a2545cf5c24ffdb
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-09-24 17:26:11 -0300

    fixing some return values

--- arrebol/src/scheduler.erl
+++ arrebol/src/scheduler.erl
@@ -21,28 +21,24 @@ handle_call({subs, Node}, _From, {[], []}) ->
 	{reply, ok, {[], [Node]}};
 
 handle_call({schedule}, _From, {[], []}) ->
-	_From ! {schedule, empty},
-	{reply, ok, {[], []}};
+	{reply, {schedule, empty}, {[], []}};
 
 handle_call({subs, Node}, _From, {[Head | Tail], []}) ->
 	{reply, ok, {[Head | Tail], [Node]}};
 
 handle_call({schedule}, _From, {[], [Head | Tail]}) ->
-	_From ! {schedule, empty},
-	{reply, ok, {[], [Head | Tail]}};
+	{reply, {schedule, empty}, {[], [Head | Tail]}};
 
 handle_call({subs, Node}, _From, {[], [Head | Tail]}) ->
 	{reply, ok, {[], [Head, lists:append(Tail, [Node])]}};
 
 handle_call({schedule}, _From, {[Head | Tail], [Whead | Wtail]}) ->
-	schedule_job(Head, _From),
-	{reply, ok, {Tail, [Whead | Wtail]}};
+	{reply, {schedule, Head, self()}, {Tail, [Whead | Wtail]}};
 
 handle_call({subs, Node}, _From, {[Head | Tail], [Whead | Wtail]}) ->
 	{reply, ok, {[Head | Tail], [Head, lists:append(Tail, [Node])]}}.
 
 handle_cast({add, Job}, {[], []}) ->
-	io:format("oiiiiiiiiiiii"),
 	{noreply, {[Job], []}};
 
 handle_cast({add, Job}, {[Head | Tail], []}) ->
@@ -53,9 +49,3 @@ handle_cast({add, Job}, {[], [Head | Tail]}) ->
 
 handle_cast({add, Job}, {[Head | Tail], [Whead | Wtail]}) ->
 	{noreply, {[Head | lists:append(Tail, [Job])], [Whead | Wtail]}}.
-
-schedule_job(Job, From) ->
-	io:format("From ~p~n", [From]),
-	io:format("Job ~p~n", [Job]),
-	From ! {schedule, Job, self()},
-	ok.
